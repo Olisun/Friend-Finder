bca8ef9825a951c320329c74c4748f34ec824ad1
Oliver Sun <olisun@mac.com>
2019-10-06 17:33:51 -0700

    finished api routes. added modal in survey. added users to fixedData array

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,15 +1,32 @@
 // Setting the survey data to an array of friends objects. Adding one intial default. 
-var userData = [{
-    name: 'Taylor Sun',
+var fixedData = [
+  // Potential new BFFs. 
+  {
+    name: 'Sister Mary Stewart',
     photo: 'https://pixabay.com/photos/nun-zombie-possessed-dark-demoniac-1623680/',
     scores: [3, 3, 3, 3, 3, 5, 5, 4, 5, 4]
   },
   {
-    name: 'Taylor Sun',
-    photo: 'https://pixabay.com/photos/nun-zombie-possessed-dark-demoniac-1623680/',
-    scores: [3, 3, 3, 3, 3, 5, 5, 4, 5, 4]
+    name: 'Tony Stark',
+    photo: 'https://pixabay.com/photos/iron-man-avengers-marvel-super-hero-4241268/',
+    scores: [5, 3, 3, 5, 5, 5, 5, 4, 5, 4]
+  },
+  {
+    name: 'Steve Rodgers',
+    photo: 'https://pixabay.com/vectors/captain-america-chris-evans-cap-4027077/',
+    scores: [5, 5, 5, 5, 5, 1, 1, 5, 5, 3]
+  },
+  {
+    name: 'Peter Parker',
+    photo: 'https://pixabay.com/illustrations/spiderman-avengers-endgame-comic-4112322/',
+    scores: [5, 3, 3, 5, 5, 5, 5, 4, 5, 4]
+  },
+  {
+    name: 'I am Groot!',
+    photo: 'https://pixabay.com/photos/groot-tree-the-grassland-model-4121381/',
+    scores: [5, 3, 3, 5, 2, 5, 5, 5, 5, 2]
   }
 ];
 
 // Making the friends data accessible to other files. 
-module.exports = userData;
+module.exports = fixedData;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,16 +1,46 @@
 // Setting the dependancies and api routes. 
 var path = require('path');
-var userData = require('../data/friends');
+var fixedData = require('../data/friends');
 
 // Setting up the api functions to be exported. 
 module.exports = function(app) {
   // Setting up a GET route with the url /api/friends. This will be used to display a JSON of all possible friends.
   app.get('/api/friends', function(request, response) {
-    response.json(newUserData);
+    response.json(fixedData);
   });
 
   // Setting up POST routes /api/friends. This will be used to handle incoming survey results. This route will also be used to handle the compatibility logic.
   app.post('/api/friends', function(request, response) {
-    console.log(newUserData);
+    var survey = request.body;
+    var surveyScores = request.body.scores;
+    // Must convert survey scores to numbers. 
+    for (var i = 0; i < surveyScores.length; i++) {
+      surveyScores[i] = parseInt(surveyScores[i]);
+    };
+
+    console.log(surveyScores);
+    // Setting the index value and the defaultBeta value as starting points. 
+    var index = 0;
+    var defaultBeta = 100;
+
+
+    //
+    for (var i = 0; i < fixedData.length; i++) {
+      console.log(fixedData[i].scores);
+      var beta = 0;
+      for (var j = 0; j < fixedData[i].scores.length; j++) {
+        var beta = (Math.abs(surveyScores[j] - fixedData[i].scores[j]));
+        defaultBeta += beta;
+      };
+    };
+    //
+    if (beta < defaultBeta) {
+      index = i;
+      defaultBeta = beta;
+    }
+    //
+    fixedData.push(request.body);
+    //
+    response.json(fixedData[index])
   })
 }
